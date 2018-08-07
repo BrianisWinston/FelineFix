f093aaa499d882cfd6a33378f452b43070dce8d2
Winston Galas <BrianisWinston@yahoo.com>
2018-08-07 13:52:05 -0700

    Create likes component and container

--- frontend/reducers/photos_reducer.js
+++ frontend/reducers/photos_reducer.js
@@ -1,6 +1,7 @@
 import { RECEIVE_ALL_PHOTOS,
          RECEIVE_PHOTO,
          REMOVE_PHOTO } from '../actions/photo_actions';
+import { ADD_LIKE, REMOVE_LIKE} from '../actions/like_actions';
 import merge from 'lodash/merge';
 
 const PhotosReducer = (oldState = {}, action) => {
@@ -9,9 +10,10 @@ const PhotosReducer = (oldState = {}, action) => {
     case RECEIVE_ALL_PHOTOS:
     console.log('photos reducer')
       return merge({}, action.photos);
+    case ADD_LIKE:
+    case REMOVE_LIKE:
     case RECEIVE_PHOTO:
     console.log('photo reducer');
-    aweqr
       return merge({}, oldState, {[action.photo.id]: action.photo});
     case REMOVE_PHOTO:
       let newState = merge({}, oldState);
