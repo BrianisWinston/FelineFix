5535f81518e0e56ccb0f1e717b109a56a8445b28
Winston Galas <BrianisWinston@yahoo.com>
2018-08-30 20:44:05 -0700

    Npm install all depedencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require("path");
 module.exports = {
   context: __dirname,
   entry: "./frontend/insta_clone.jsx",
+  watch: true,
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "./bundle.js"
