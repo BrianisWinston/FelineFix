30aa9072c7a8b4a22fe0e8acbcce80ac1258d317
Winston Galas <BrianisWinston@yahoo.com>
2018-08-30 21:16:52 -0700

    Add reducer like logic

--- frontend/actions/like_actions.js
+++ frontend/actions/like_actions.js
@@ -3,7 +3,19 @@ import * as LikeApiUtil from '../util/like';
 export const ADD_LIKE = 'ADD_LIKE';
 export const REMOVE_LIKE = 'REMOVE_LIKE';
 
-const addLike = photo => ({
+export const addLike = photo_id => dispatch => {
+  console.log('add like actions');
+
+  LikeApiUtil.addLike(photo_id).then(photo => dispatch(createLike(photo)))
+};
+
+export const deleteLike = photo_id => dispatch => {
+  console.log('delete like actions');
+
+  LikeApiUtil.deleteLike(photo_id).then(photo => dispatch(removeLike(photo)));
+};
+
+const createLike = photo => ({
   type: ADD_LIKE,
   photo
 });
--- frontend/reducers/photos_reducer.js
+++ frontend/reducers/photos_reducer.js
@@ -1,14 +1,20 @@
 import { RECEIVE_ALL_PHOTOS,
          RECEIVE_PHOTO,
          REMOVE_PHOTO } from '../actions/photo_actions';
+import { ADD_LIKE, REMOVE_LIKE} from '../actions/like_actions';
 import merge from 'lodash/merge';
 
 const PhotosReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
+  let newState;
   switch (action.type) {
     case RECEIVE_ALL_PHOTOS:
+    console.log('photos reducer')
       return merge({}, action.photos);
+    case ADD_LIKE:
+    case REMOVE_LIKE:
     case RECEIVE_PHOTO:
+    console.log('photo reducer');
       return merge({}, oldState, {[action.photo.id]: action.photo});
     case REMOVE_PHOTO:
       let newState = merge({}, oldState);
