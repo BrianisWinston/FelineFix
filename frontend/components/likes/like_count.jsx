e873aead61a4fecb57c131946803832ac437b3d7
Winston Galas <BrianisWinston@yahoo.com>
2018-09-05 01:23:23 -0700

    Add different like outputs

--- frontend/components/likes/like_count.jsx
+++ frontend/components/likes/like_count.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const LikeCount = ({photo, likeState}) => {
+  let likeStatus = "ye"
 
   if (photo.people_liked.length === 1 && likeState) {
-    const likeStatus = "Liked by you";
+    likeStatus = "Liked by you";
   } else if (photo.people_liked.length === 2 && likeState) {
-    const likeStatus = `Liked by you and ${photo.people_liked.length - 1} other`
+    likeStatus = "Liked by you and 1 other"
   } else if (photo.people_liked.length < 2 && likeState) {
-    const likeStatus =
+    likeStatus = `Liked by you and ${photo.people_liked.length - 1} others`
+  } else {
+    likeStatus = `${photo.people_liked.length} likes`
   };
 
   return (
@@ -20,7 +23,7 @@ const LikeCount = ({photo, likeState}) => {
 
 const mapStateToProps = (state, ownProps) => {
   const likeState = ownProps.photo.people_liked.includes(state.session.currentUser.username)
-  return likeState;
+  return { likeState, };
 }
 
 export default connect(mapStateToProps)(LikeCount);
