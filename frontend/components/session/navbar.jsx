8f558d6dafee9ef9246a5f065f4a6bda3d9b1c16
Winston Galas <BrianisWinston@yahoo.com>
2018-11-01 00:28:39 -0700

    Add scroll to top functionality when clicking on logo

--- frontend/components/session/navbar.jsx
+++ frontend/components/session/navbar.jsx
@@ -4,18 +4,24 @@ import UserProfile from './user_profile';
 import { Link } from 'react-router-dom';
 
 export default ({ logout, currentUser, createPhoto }) => {
+
+  const scrollUp = () => {
+    scrollTo(0, 100);
+  }
   return (
     <header>
       <div className="nav">
         <div className="navbar">
           <div className="leftside">
             <Link to='/'>
+            <button onClick={scrollUp()}>
               <div className="icon-felinefix">
                 <i className="fas fa-paw"></i>
               </div>
               <div className="icon-logo">
                 FelineFix
               </div>
+              </button>
             </Link>
           </div>
           <div className="middle">
